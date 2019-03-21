549ed696dc2ad5d5986e98980e85f302f5ae1e2a
angelmorexp <angelmorexp@gmail.com>
2019-03-21 19:23:48 +0000

    Subo práctica E2.2, mejorada

---
+++ MF0492_3 SERVIDOR/UF1844/E2.2/src/Acceso.java
@@ -0,0 +1,107 @@
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+
+import java.awt.Font;
+import javax.swing.JTextField;
+import javax.swing.JButton;
+import javax.swing.JPasswordField;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import java.awt.Color;
+
+public class Acceso extends JFrame {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	private JPanel contentPane;
+	private JTextField textField;
+	private JButton btnAceptar;
+	private JPasswordField passwordField;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					Acceso frame = new Acceso();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public Acceso() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 323, 187);
+		contentPane = new JPanel();
+		contentPane.setBackground(new Color(255, 240, 245));
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		setTitle("Acceso");
+		
+		JLabel lblUsuario = new JLabel("Usuario (admin)");
+		lblUsuario.setFont(new Font("Tahoma", Font.BOLD, 14));
+		lblUsuario.setBounds(10, 11, 112, 17);
+		contentPane.add(lblUsuario);
+		
+		textField = new JTextField();
+		textField.setFont(new Font("Tahoma", Font.PLAIN, 14));
+		textField.setBounds(10, 35, 168, 21);
+		contentPane.add(textField);
+		textField.setColumns(10);
+		
+		JLabel label = new JLabel("Contraseña (admin123)");
+		label.setFont(new Font("Tahoma", Font.BOLD, 14));
+		label.setBounds(10, 77, 168, 17);
+		contentPane.add(label);
+		
+		passwordField = new JPasswordField();
+		passwordField.setBackground(new Color(255, 255, 255));{
+		passwordField.setBounds(10, 100, 168, 21);
+		contentPane.add(passwordField);
+		
+		
+		btnAceptar = new JButton("ACEPTAR");
+		btnAceptar.setFont(new Font("Tahoma", Font.BOLD, 11));
+		btnAceptar.setForeground(new Color(255, 240, 245));
+		btnAceptar.setBackground(new Color(219, 112, 147));
+		btnAceptar.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				
+				String usuario = textField.getText();
+				char clave[] = passwordField.getPassword();
+				String contraseña= new String(clave);
+				if (usuario.equals("admin")&& contraseña.equals("admin123")) {
+					
+					setVisible(false);  //ESCONDER EL LOGIN
+					
+					Formulario nw = new Formulario(); //MOSTRAR EL FORMULARIO
+					
+				}
+				else {
+					JOptionPane.showMessageDialog(null, "Usuario o Contraseña Inválidos"); //MOSTRAR QUE EL USUARIO O LA CONTRASEÑA ES ERRONEA
+				}
+			}
+		});
+		btnAceptar.setBounds(208, 35, 89, 87);
+		contentPane.add(btnAceptar);
+		
+		}
+	}
+}
---
+++ MF0492_3 SERVIDOR/UF1844/E2.2/src/Formulario.java
@@ -0,0 +1,207 @@
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+import javax.swing.*;
+import javax.swing.border.EmptyBorder;
+import java.awt.Font;
+import java.awt.Color;
+import java.awt.event.ActionListener;
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+import java.io.UnsupportedEncodingException;
+import java.awt.event.ActionEvent;
+
+
+
+public class Formulario extends JFrame {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	private JPanel contentPane;
+	private JTextField textField;
+	private JTextField textField_1;
+	private JTextField textField_2;
+	private JTextField textField_3;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void NewScreen() {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					Formulario frame = new Formulario();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public Formulario() {
+		//Añade un titulo, no es estrictamente necesario
+		setTitle("Formulario");
+
+		/*
+		 * Coordenadas x y de la aplicacion y su altura y longitud,
+		 * si no lo indicamos aparecera una ventana muy pequeña
+		 */
+		setBounds(400, 200, 564, 311);
+
+		/*
+		 * Indica que cuando se cierre la ventana se acaba la aplicacion,
+		 * si no lo indicamos cuando cerremos la ventana la aplicacion seguira funcionando
+		 */
+		setDefaultCloseOperation(EXIT_ON_CLOSE);
+
+		//Hace visible la ventana, si no lo hacemos no veremos la aplicacion
+		setVisible(true);
+
+		//Creamos el panel
+		contentPane =new JPanel();
+		contentPane.setBackground(new Color(255, 240, 245));
+
+		//Indicamos su diseño
+		contentPane.setLayout(null);
+
+		//asigno el pannel a la ventana
+		setContentPane(contentPane);
+
+		JLabel lblNombre = new JLabel("Nombre");
+		lblNombre.setFont(new Font("Tahoma", Font.BOLD, 14));
+		lblNombre.setBounds(20, 22, 121, 22);
+		contentPane.add(lblNombre);
+
+		textField_2 = new JTextField();
+		textField_2.setBounds(20, 44, 121, 22);
+		contentPane.add(textField_2);
+		textField_2.setColumns(10);
+
+		JLabel label = new JLabel("Apellidos");
+		label.setFont(new Font("Tahoma", Font.BOLD, 14));
+		label.setBounds(20, 71, 121, 22);
+		contentPane.add(label);
+
+		textField_3 = new JTextField();
+		textField_3.setColumns(10);
+		textField_3.setBounds(20, 96, 121, 22);
+		contentPane.add(textField_3);
+		
+		JLabel label_1 = new JLabel("Edad");
+		label_1.setFont(new Font("Tahoma", Font.BOLD, 14));
+		label_1.setBounds(164, 129, 58, 22);
+		contentPane.add(label_1);
+
+		JSpinner spinner = new JSpinner();
+		spinner.setModel(new SpinnerNumberModel(0, 0, 200, 1));
+		spinner.setBounds(164, 158, 70, 22);
+		contentPane.add(spinner);
+
+		JLabel label_2 = new JLabel("Género");
+		label_2.setFont(new Font("Tahoma", Font.BOLD, 14));
+		label_2.setBounds(164, 22, 58, 22);
+		contentPane.add(label_2);
+
+		JRadioButton rdbtnNewRadioButton = new JRadioButton("Hombre");
+		rdbtnNewRadioButton.setBackground(new Color(255, 240, 245));
+		rdbtnNewRadioButton.setBounds(164, 70, 109, 23);
+		contentPane.add(rdbtnNewRadioButton);
+
+		JRadioButton rdbtnNewRadioButton_1 = new JRadioButton("Mujer");
+		rdbtnNewRadioButton_1.setBackground(new Color(255, 240, 245));
+		rdbtnNewRadioButton_1.setBounds(164, 44, 109, 23);
+		contentPane.add(rdbtnNewRadioButton_1);
+
+		JRadioButton rdbtnNewRadioButton_2 = new JRadioButton("No Binario");
+		rdbtnNewRadioButton_2.setBackground(new Color(255, 240, 245));
+		rdbtnNewRadioButton_2.setBounds(164, 96, 109, 23);
+		contentPane.add(rdbtnNewRadioButton_2);
+
+		ButtonGroup bgroup = new ButtonGroup();
+		bgroup.add(rdbtnNewRadioButton);
+		bgroup.add(rdbtnNewRadioButton_1);
+		bgroup.add(rdbtnNewRadioButton_2);
+		
+		JLabel label_3 = new JLabel("Ocupación");
+		label_3.setFont(new Font("Tahoma", Font.BOLD, 14));
+		label_3.setBounds(20, 129, 121, 22);
+		contentPane.add(label_3);
+
+		ButtonGroup bgroup1 = new ButtonGroup();
+
+		JCheckBox chckbxEstudiante = new JCheckBox("Estudiante");
+		chckbxEstudiante.setBackground(new Color(255, 240, 245));
+		chckbxEstudiante.setBounds(20, 158, 97, 23);
+		contentPane.add(chckbxEstudiante);
+
+		JCheckBox chckbxTrabajador = new JCheckBox("Trabajador");
+		chckbxTrabajador.setBackground(new Color(255, 240, 245));
+		chckbxTrabajador.setBounds(20, 184, 97, 23);
+		contentPane.add(chckbxTrabajador);
+		
+		JLabel lblOtrosDatosDe = new JLabel("Otros datos de Inter\u00E9s");
+		lblOtrosDatosDe.setFont(new Font("Tahoma", Font.BOLD, 14));
+		lblOtrosDatosDe.setBounds(279, 26, 183, 14);
+		contentPane.add(lblOtrosDatosDe);
+
+		JTextArea textArea = new JTextArea();
+		textArea.setForeground(new Color(219, 112, 147));
+		textArea.setBackground(Color.WHITE);
+		textArea.setBounds(279, 51, 238, 156);
+		contentPane.add(textArea);
+		
+		JButton btnEnviar = new JButton("ENVIAR");
+		btnEnviar.setFont(new Font("Tahoma", Font.BOLD, 11));
+		btnEnviar.setBackground(new Color(219, 112, 147));
+		btnEnviar.setForeground(new Color(255, 240, 245));
+		btnEnviar.addActionListener(new ActionListener(){
+			   public void actionPerformed(ActionEvent arg0){
+				   String nombre= textField_2.getText();
+				   String apellido= textField_3.getText();
+				   Integer edad= (Integer)spinner.getValue();
+				   String genero = null;
+					if (rdbtnNewRadioButton.isSelected()) { 
+						genero = "Hombre";
+					} else if (rdbtnNewRadioButton_1.isSelected()){
+						genero = "Mujer";
+					} else if (rdbtnNewRadioButton_2.isSelected()) {
+						genero = "No binario";
+					} else {
+						genero = "No indicado";
+					}
+					String ocupacion = null;
+					
+					if (chckbxTrabajador.isSelected() && chckbxEstudiante.isSelected()) {
+						ocupacion= "Estudiante y Trabajador";
+					}else if (chckbxEstudiante.isSelected()) {
+						ocupacion= "Estudiante";
+					}else if (chckbxTrabajador.isSelected()) {
+						ocupacion= "Trabajador";
+					}else {
+						ocupacion= "Ni estudio ni trabajo";
+					}
+					String datos= textArea.getText();
+					String mensaje= " Nombre: "+nombre+"\n Apellidos: "+apellido+"\n Edad: "+edad+"\n Genero: "+genero+"\n Ocupación: "+ocupacion+"\n Otros datos: "+datos;
+					JOptionPane.showMessageDialog(null, mensaje ); //MOSTRAR LOS DATOS DEL FORMULARIO
+					PrintWriter writer = null;
+					try {
+						writer = new PrintWriter("usuario.txt", "UTF-8");				//CREAR UN ARCHIVO .TXT CON LOS DATOS DE USUARIO
+					} catch (FileNotFoundException | UnsupportedEncodingException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					}
+					writer.println(mensaje);
+					writer.close();
+			   }
+			});
+		btnEnviar.setBounds(229, 238, 89, 23);
+		contentPane.add(btnEnviar);
+
+
+	}
+}
