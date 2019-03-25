d921402c2b7f55c29e018ee0cc49f45d945d9442
angelmorexp <angelmorexp@gmail.com>
2019-03-25 17:28:33 +0000

    E2.2 Actualizada. Ahora comprueba si el nombre del archivo .txt generado existe, y en tal caso asigna uno nuevo.

--- MF0492_3 SERVIDOR/UF1844/E2.2/src/Formulario.java
+++ MF0492_3 SERVIDOR/UF1844/E2.2/src/Formulario.java
@@ -1,10 +1,12 @@
 import java.awt.BorderLayout;
 import java.awt.EventQueue;
 import javax.swing.*;
+import javax.swing.border.Border;
 import javax.swing.border.EmptyBorder;
 import java.awt.Font;
 import java.awt.Color;
 import java.awt.event.ActionListener;
+import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.PrintWriter;
 import java.io.UnsupportedEncodingException;
@@ -21,8 +23,8 @@ public class Formulario extends JFrame {
 	private JPanel contentPane;
 	private JTextField textField;
 	private JTextField textField_1;
-	private JTextField textField_2;
-	private JTextField textField_3;
+	private JTextField txtNoIndicado;
+	private JTextField txtNoIndicado_1;
 
 	/**
 	 * Launch the application.
@@ -77,20 +79,22 @@ public class Formulario extends JFrame {
 		lblNombre.setBounds(20, 22, 121, 22);
 		contentPane.add(lblNombre);
 
-		textField_2 = new JTextField();
-		textField_2.setBounds(20, 44, 121, 22);
-		contentPane.add(textField_2);
-		textField_2.setColumns(10);
+		txtNoIndicado = new JTextField();
+		txtNoIndicado.setText("No indicado");
+		txtNoIndicado.setBounds(20, 44, 121, 22);
+		contentPane.add(txtNoIndicado);
+		txtNoIndicado.setColumns(10);
 
 		JLabel label = new JLabel("Apellidos");
 		label.setFont(new Font("Tahoma", Font.BOLD, 14));
 		label.setBounds(20, 71, 121, 22);
 		contentPane.add(label);
 
-		textField_3 = new JTextField();
-		textField_3.setColumns(10);
-		textField_3.setBounds(20, 96, 121, 22);
-		contentPane.add(textField_3);
+		txtNoIndicado_1 = new JTextField();
+		txtNoIndicado_1.setText("No indicado");
+		txtNoIndicado_1.setColumns(10);
+		txtNoIndicado_1.setBounds(20, 96, 121, 22);
+		contentPane.add(txtNoIndicado_1);
 		
 		JLabel label_1 = new JLabel("Edad");
 		label_1.setFont(new Font("Tahoma", Font.BOLD, 14));
@@ -150,12 +154,12 @@ public class Formulario extends JFrame {
 		contentPane.add(lblOtrosDatosDe);
 
 		JTextArea textArea = new JTextArea();
+		Border border = BorderFactory.createLineBorder(Color.black, 1);
+		textArea.setBorder(border);
 		textArea.setForeground(new Color(219, 112, 147));
 		textArea.setBackground(Color.WHITE);
 		textArea.setBounds(279, 51, 238, 156);
 		contentPane.add(textArea);
-		
-		int [] contador= {1};
 		
 		JButton btnEnviar = new JButton("ENVIAR");
 		btnEnviar.setFont(new Font("Tahoma", Font.BOLD, 11));
@@ -163,8 +167,8 @@ public class Formulario extends JFrame {
 		btnEnviar.setForeground(new Color(255, 240, 245));
 		btnEnviar.addActionListener(new ActionListener(){
 			public void actionPerformed(ActionEvent arg0){
-				   String nombre= textField_2.getText();
-				   String apellido= textField_3.getText();
+				   String nombre= txtNoIndicado.getText();
+				   String apellido= txtNoIndicado_1.getText();
 				   Integer edad= (Integer)spinner.getValue();
 				   String genero = null;
 					if (rdbtnNewRadioButton.isSelected()) { 
@@ -193,16 +197,24 @@ public class Formulario extends JFrame {
 					PrintWriter writer = null;
 					
 					try {
-						writer = new PrintWriter("usuario"+contador[0]+".txt", "UTF-8");				//CREAR UN ARCHIVO .TXT CON LOS DATOS DE USUARIO
-					} 
+						boolean exists= true;
+						int contador = 0;
+						
+						while (exists) {			//BUCLE QUE COMPRUEBA LA EXISTENCIA DEL NOMBRE DEL DOCUMENTO, SI NO EXISTE, SE TERMINA EL BUCLE
+							contador++;
+							File tempFile = new File("C:/Users/aula1/eclipse-workspace/E2.2/usuario_"+contador+".txt");		//VARIABLE DEL NOMBRE DEL ARCHIVO
+							exists = tempFile.exists();					//COMPRUEBA ARCHIVO EXISTENTE
+						}
+						String documento= "usuario_"+contador+".txt";	//NOMBRE DEL DOCUMENTO NUEVO
+						
+						writer = new PrintWriter(documento, "UTF-8");	//CREAR UN ARCHIVO .TXT CON LOS DATOS DE USUARIO
+					}
 					catch (FileNotFoundException | UnsupportedEncodingException e) {
 						// TODO Auto-generated catch block
 						e.printStackTrace();
 					}
 					writer.println(mensaje);
 					writer.close();
-					contador[0]++;
-					System.out.println(contador[0]);
 			   }
 			   
 			});
